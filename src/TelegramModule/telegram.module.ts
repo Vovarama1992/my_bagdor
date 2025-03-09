a3c02cfbd5eec6bc3298ea5805296f05499886e6
Vovarama1992 <vovayhh9988@gmail.com>
2025-03-09 08:56:27 +0300

    'fix'

--- src/TelegramModule/telegram.module.ts
+++ src/TelegramModule/telegram.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TelegramService } from './telegram.service';
+import { PrismaModule } from 'src/PrismaModule/prisma.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, PrismaModule],
   providers: [TelegramService],
   exports: [TelegramService],
 })

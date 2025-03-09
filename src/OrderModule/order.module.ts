0f4533706822ae41967f572df2a4aebfe4a61c52
Vovarama1992 <vovayhh9988@gmail.com>
2025-03-09 11:43:00 +0300

    'fix'

--- src/OrderModule/order.module.ts
+++ src/OrderModule/order.module.ts
@@ -3,11 +3,10 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaModule } from 'src/PrismaModule/prisma.module';
 import { UsersModule } from 'src/UserModule/users.module';
-import { FlightModule } from 'src/FlightModule/flight.module';
 import { TelegramModule } from 'src/TelegramModule/telegram.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, FlightModule, TelegramModule],
+  imports: [PrismaModule, UsersModule, TelegramModule],
   controllers: [OrderController],
   providers: [OrderService],
 })

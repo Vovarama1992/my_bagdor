e5cd823e7c2767accc92d5f821a599fe7c3022a8
Vovarama1992 <vovayhh9988@gmail.com>
2025-03-15 12:39:47 +0300

    'fix'

--- src/JwtModule/jwt.module.ts
+++ src/JwtModule/jwt.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { JwtModule as NestJwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -7,10 +7,23 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule,
     NestJwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '24h' },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const jwtSecret = configService.get<string>('JWT_SECRET');
+
+        // Логируем `JWT_SECRET`
+        const logger = new Logger('JwtModule');
+        logger.log(`🔑 JWT_SECRET: ${jwtSecret ? 'ЗАГРУЖЕН' : 'НЕ НАЙДЕН'}`);
+
+        if (!jwtSecret) {
+          logger.error('❌ JWT_SECRET отсутствует в .env!');
+          throw new Error('❌ JWT_SECRET отсутствует в .env!');
+        }
+
+        return {
+          secret: jwtSecret,
+          signOptions: { expiresIn: '24h' },
+        };
+      },
       inject: [ConfigService],
     }),
   ],
